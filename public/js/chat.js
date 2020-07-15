e84270bdd977c18c5f112565c9878ff2cae4424d
TANUMON ROY <roytanumon@gmail.com>
2020-07-16 00:23:56 +0530

    added msg rendering and acknowlegments

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,19 +1,56 @@
 const socket = io();
 
 socket.on("message", (msg) => {
-   console.log(msg);
+    console.log(msg);
 });
 
-const msgForm = document.querySelector("form");
+const $msgForm = document.querySelector("form");
+const $msgFormInput = $msgForm.querySelector('input')
+const $msgFormButton = $msgForm.querySelector('button')
+const $sendLoc = document.querySelector("#send-location")
+const $messages = document.querySelector('#messages')
 
-msgForm.addEventListener("submit", (e) => {
-   e.preventDefault();
-   const msg = e.target.elements.message;
+const $msgTemplate = document.querySelector('#message-template').innerHTML
 
-   console.log("Message sent");
-   socket.emit("sendMsg", msg.value);
+$msgForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    $msgFormButton.setAttribute('disabled', 'disabled')
+    const msg = e.target.elements.message.value;
+
+    socket.emit("sendMsg", msg, (error) => {
+        $msgFormButton.removeAttribute('disabled')
+        $msgFormInput.value = ''
+        $msgFormInput.focus()
+        if (error) {
+            console.log(error)
+        } else {
+            console.log("Delivered!");
+        }
+    });
 });
 
 socket.on("updateMsg", (msg) => {
-   console.log(msg);
+    console.log(msg);
+    const html = Mustache.render($msgTemplate, { msg })
+    $messages.insertAdjacentHTML('beforeend', html)
+});
+
+$sendLoc.addEventListener("click", () => {
+    if (!navigator.geolocation) {
+        return alert("Geolocation is not supported by your browser");
+    }
+    $sendLoc.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition((position) => {
+        socket.emit("sendLocation", {
+            lat: position.coords.latitude,
+            long: position.coords.longitude,
+        }, (error) => {
+            if (error) {
+                console.log(error)
+            } else {
+                console.log('Location Shared!')
+            }
+        });
+    });
+    $sendLoc.removeAttribute('disabled')
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const http = require("http");
 const path = require("path");
 const express = require("express");
 const socketio = require("socket.io");
+const Filter = require('bad-words')
 
 const publicDir = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
@@ -13,14 +14,33 @@ const io = socketio(server);
 app.use(express.static(path.join(publicDir)));
 
 io.on("connection", (socket) => {
-   console.log("New WebSocket connection");
-   socket.emit("message", "Welcome!");
+    console.log("New WebSocket connection");
 
-   socket.on("sendMsg", (msg) => {
-      io.emit("updateMsg", msg);
-   });
+    socket.emit("message", "Welcome!");
+    socket.broadcast.emit("message", "A new user has joined!");
+
+    socket.on("sendMsg", (msg, callback) => {
+        const filter = new Filter()
+        if (filter.isProfane(msg)) {
+            return callback('Profanity is not allowed.')
+        }
+        io.emit("updateMsg", msg);
+        callback();
+    });
+
+    socket.on("sendLocation", ({ lat, long }, callback) => {
+        socket.broadcast.emit(
+            "message",
+            "https://google.com/maps?q=" + lat + "," + long
+        );
+        callback()
+    });
+
+    socket.on("disconnect", () => {
+        io.emit("message", "A User has left.");
+    });
 });
 
 server.listen(port, () => {
-   console.log("Server is up on port " + port);
+    console.log("Server is up on port " + port);
 });
